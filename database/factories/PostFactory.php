b4b9516c30c70e472704fe8d5cbdf7c75dd8306e
Nelson <juniormoncada17@gmail.com>
2022-07-19 18:24:48 +0200

    Add relation post belongs to user

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -2,6 +2,8 @@
 
 namespace Database\Factories;
 
+use App\Models\Category;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -17,7 +19,9 @@ class PostFactory extends Factory
     public function definition()
     {
         return [
-            //
+
+            'user_id' => User::factory(),
+            'category_id' => Category::factory()
         ];
     }
 }
