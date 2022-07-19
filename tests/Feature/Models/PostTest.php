93e2f3b670b892c46ad8569485a7205e9f51439f
Nelson <juniormoncada17@gmail.com>
2022-07-19 18:19:13 +0200

    Created PostTest

--- tests/Feature/Models/PostTest.php
+++ tests/Feature/Models/PostTest.php
@@ -2,21 +2,24 @@
 
 namespace Tests\Feature\Models;
 
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class PostTest extends TestCase
 {
+    use RefreshDatabase;
     /**
      * A basic feature test example.
      *
      * @return void
      */
-    public function test_example()
+    public function test_posts_belong_to_user()
     {
-        $response = $this->get('/');
+        $post = Post::factory()->create();
 
-        $response->assertStatus(200);
+        $this->assertInstanceOf(User::class, $post->user);
     }
 }
