3d115aca01f27b4734d8587696a78ec23c2654c4
Nelson <juniormoncada17@gmail.com>
2022-07-19 20:03:14 +0200

    add relation posts belongs to category and post has many comments

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 use App\Models\User;
+use App\Models\Category;
+use App\Models\Comment;
 
 class Post extends Model
 {
@@ -14,5 +16,15 @@ class Post extends Model
     public function user(){
 
         return $this->belongsTo(User::class);
+    }
+    
+    public function category(){
+
+        return $this->belongsTo(Category::class);
+    }
+    
+    public function comment(){
+
+        return $this->hasMany(Comment::class);
     }
 }
--- tests/Feature/Models/PostTest.php
+++ tests/Feature/Models/PostTest.php
@@ -2,8 +2,11 @@
 
 namespace Tests\Feature\Models;
 
+use App\Models\Category;
+use App\Models\Comment;
 use App\Models\Post;
 use App\Models\User;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -21,5 +24,19 @@ class PostTest extends TestCase
         $post = Post::factory()->create();
 
         $this->assertInstanceOf(User::class, $post->user);
+    }
+    
+    public function test_posts_belong_to_category()
+    {
+        $post = Post::factory()->create();
+
+        $this->assertInstanceOf(Category::class, $post->category);
+    }
+
+    public function test_post_has_many_comments(){
+
+        $post = Post::factory()->create();
+
+        $this->assertInstanceOf(Collection::class, $post->comment);
     }
 }
