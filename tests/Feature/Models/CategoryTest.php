48ee6fee7b4d40aee9dc07aae845b05af0c2093b
Nelson <juniormoncada17@gmail.com>
2022-07-19 18:07:48 +0200

    Add relation category has many posts

--- tests/Feature/Models/CategoryTest.php
+++ tests/Feature/Models/CategoryTest.php
@@ -6,17 +6,22 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
+use App\Models\Category;
+use Illuminate\Database\Eloquent\Collection;
+
 class CategoryTest extends TestCase
 {
+    use RefreshDatabase;
     /**
      * A basic feature test example.
      *
      * @return void
      */
-    public function test_example()
+    public function test_category_has_many_posts()
     {
-        $response = $this->get('/');
+        $category = Category::factory()->create();
 
-        $response->assertStatus(200);
+        $this->assertInstanceOf(Collection::class, $category->post);
+
     }
 }
