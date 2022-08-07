4c33e16426cef6c40b00773b68ca831bda2f1e95
Nelson <juniormoncada17@gmail.com>
2022-08-07 17:59:28 +0200

    Test posts has required columns

--- tests/Feature/Models/PostTest.php
+++ tests/Feature/Models/PostTest.php
@@ -9,6 +9,7 @@ use App\Models\User;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Schema;
 use Tests\TestCase;
 
 class PostTest extends TestCase
@@ -38,5 +39,19 @@ class PostTest extends TestCase
         $post = Post::factory()->create();
 
         $this->assertInstanceOf(Collection::class, $post->comment);
+    }
+
+    public function test_posts_table_has_required_columns(){
+
+        $schema = Schema::getColumnListing('posts');
+
+            $this->assertContains('id', $schema);
+            $this->assertContains('title', $schema);
+            $this->assertContains('published_at', $schema);
+            $this->assertContains('content', $schema);
+            $this->assertContains('status', $schema);
+            $this->assertContains('users_id', $schema);
+            $this->assertContains('categories_id', $schema);
+
     }
 }
